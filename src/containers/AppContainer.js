bcc59e07c87ea3bedb89c024c9aa31e2f31e71f2
Daniel Bartley <danielbartley1999@gmail.com>
2020-06-04 04:22:31 +0000

    changed from checking API for US zip code to NZ city name

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -7,9 +7,9 @@ function AppContainer(props) {
 
     const [responseData, setResponseData] = useState('');
 
-    const handleZipChange = async (zipValue) => {
-        //console.log(`--------- fetchData called zip:${zipValue}`)
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial&zip=${zipValue},us`)
+    const handleNameChange = async (cityName) => {
+        //console.log(`--------- fetchData called zip:${cityName}`)
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial&q=${cityName},nz`)
         const json = await res.json()
         //console.log(json);
         setResponseData(json);
@@ -23,7 +23,7 @@ function AppContainer(props) {
         <div>
             <div className="row mt-4">
                 <div className="col-sm-4"></div>
-                <Zip onNameChange={handleZipChange} clearResponse={clearResponse}/>
+                <Zip onNameChange={handleNameChange} clearResponse={clearResponse}/>
                 <div className="col-sm-4"></div>
             </div>
             <div className="row mt-4">
