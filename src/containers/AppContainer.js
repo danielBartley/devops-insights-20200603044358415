e7af5bf0a109dada23ae64ebf18506e29dd184f3
Daniel Bartley <danielbartley1999@gmail.com>
2020-06-05 02:46:14 +0000

    changed map code location

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -30,7 +30,18 @@ function AppContainer(props) {
                 <div className="col-sm-2"></div>
                 <ZipResponse responseData={responseData} clearResponse={clearResponse}/>
                 <div className="col-sm-2"></div>
-            </div>    
+            </div> 
+            <div id="map"></div>
+    <script>
+function initMap() {
+  var uluru = {lat: -25.344, lng: 131.036};
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBEXgPWV0cWc_MHvNQP31vl9SPml19G1o0&callback=initMap">
+    </script>
         </div>
     );
 }
