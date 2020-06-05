875d342ec9caa335f8a47292ff5e1f1ea174d348
Daniel Bartley <danielbartley1999@gmail.com>
2020-06-05 00:56:16 +0000

    edited map code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
       <AppHeader />
       <AppContainer />
-      <div id="map"></div>
+      
+       <div id="map"></div>
     <script>
 // Initialize and add the map
 function initMap() {
@@ -22,14 +23,15 @@ function initMap() {
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
     </script>
-    <!--Load the API from the specified URL
+    /*Load the API from the specified URL
     * The async attribute allows the browser to render the page while the API loads
     * The key parameter will contain your own API key (which is not needed for this tutorial)
     * The callback parameter executes the initMap() function
-    -->
+    */
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
     </script>
+      
     </div>
   );
 }
