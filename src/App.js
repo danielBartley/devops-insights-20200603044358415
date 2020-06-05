a408b4912152a6f52949edc12045d7e703312f5e
Daniel Bartley <danielbartley1999@gmail.com>
2020-06-05 02:45:45 +0000

    testing changing map code location

--- src/App.js
+++ src/App.js
@@ -12,30 +12,10 @@ function App() {
     </div>
     <div id="map"></div>
     <script>
-// Initialize and add the map
 function initMap() {
-  // The location of Uluru
   var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBEXgPWV0cWc_MHvNQP31vl9SPml19G1o0&callback=initMap">
-    </script>
-    <div id="map"></div>
-    <script>
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
     </script>
