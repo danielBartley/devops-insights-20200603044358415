b945ffc99880111dd599c2b3c283fae75223478c
Daniel Bartley <danielbartley1999@gmail.com>
2020-06-05 01:35:31 +0000

    Testing

--- src/App.js
+++ src/App.js
@@ -9,29 +9,6 @@ function App() {
     <div className="App">
       <AppHeader />
       <AppContainer />
-      
-  /*     <div id="map"></div>
-    <script>
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
-    </script>
-    /*Load the API from the specified URL
-    * The async attribute allows the browser to render the page while the API loads
-    * The key parameter will contain your own API key (which is not needed for this tutorial)
-    * The callback parameter executes the initMap() function
-    */
-/*    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBEXgPWV0cWc_MHvNQP31vl9SPml19G1o0&callback=initMap">
-    </script>*/
-      
     </div>
   );
 }
